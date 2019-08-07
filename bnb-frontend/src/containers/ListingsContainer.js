1dcf75a9cf7afac5e65257b4bc8c5fff409c2b52
sachinm78 <sachinm78@gmail.com>
2019-08-06 19:06:28 -0500

    connected bnb-api to frontend

--- bnb-frontend/src/containers/ListingsContainer.js
+++ bnb-frontend/src/containers/ListingsContainer.js
@@ -7,15 +7,26 @@ export default class ListingsContainer extends Component {
     }
 
     componentDidMount() {
-        fetch('http://localhost:3001')
+        fetch('http://localhost:3001/listings')
             .then(res => res.json())
             .then(listings => this.setState({ listings }))
     }
     render() {
         if (this.state.listings.length === 0) {
             return <h1>Loading...</h1>
-
         }
+        return (
+            <div>
+                <ul>
+                    {this.state.listings.map(listing => 
+                    <div key={listing.id}>
+                        <h4>Name: {listing.name}</h4>
+                        <p>Address: {listing.address}</p>
+                        <p>Price: ${listing.price}</p>
+                    </div>)}
+                </ul>
+            </div>
+        )
     
     }
 }
